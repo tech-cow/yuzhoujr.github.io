46c52169b1385d75465ecf75667c4333e5201c99
Yu Zhou <junior147147@yahoo.com>
2017-01-10 17:52:53 -0800

    random color update

--- project/colorGame/color.js
+++ project/colorGame/color.js
@@ -1,20 +1,14 @@
 // console.log("bannana");
 
 /***Local Variables***/
-var colors=[
-"rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
+var picked_color = pickColor();
+
 
 /****QS Variables****/
 var squares = document.querySelectorAll(".square");
-var picked_color = colors[3];
 var color_display = document.querySelector('#color_display');
-
+var message_display = document.querySelector('#message');
 
 /***Main***/
 color_display.textContent = picked_color;
@@ -29,10 +23,46 @@ for (var i = 0; i < squares.length; i++) {
     var clicked_color = this.style.background;
     //compare clicked color to picked color
     if (clicked_color === picked_color) {
-      console.log("yo");
+      message_display.textContent = "Correct!";
+      // change all color
+      change_color(clicked_color);
+
     }else {
-      console.log("no");
+      this.style.background = "#232323";
+      message_display.textContent = "Wrong!";
     }
   });
 
+}
+
+/***Function***/
+function change_color(color){
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = color;
+  }
+}
+
+
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num){
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    //get random color and push to array
+    arr.push(randomColors());
+  }
+  return arr;
+}
+
+function randomColors(){
+  //pick a red from 0 - 255
+  var r = Math.floor(Math.random() * 256);
+  // pick a green from 0 - 255
+  var g = Math.floor(Math.random() * 256);
+  // pick a blue from 0 - 255
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
