b8c556b46542d85d989aa77d86c71244a65cfe9b
Yu Zhou <junior147147@yahoo.com>
2018-04-15 21:30:14 -0700

    temp is ready

--- project-gallery/data/ui_data.js
+++ project-gallery/data/ui_data.js
@@ -1,6 +1,6 @@
 var UI_DATA = {
     "title": "Yu Zhou's Portfolio",
-    "github": "https://github.com/nekocode",
-    "description": "I'm a full-stack engineer and specialize in android development. I like open source. Here are some projects I have contributed to the community.",
-    "footer": "© Mark Yueng 2018 | <a href=\"https://github.com/nekocode/project-gallery\" target=\"_blank\">Source Code<\a>"
+    "github": "https://github.com/yuzhoujr",
+    "description": "I'm a full-stack engineer and specialize in things that are python related. I like open source. Here are some side-projects I have working on for fun.",
+    "footer": "© Yu Zhou 2018 | <a href=\"https://github.com/yuzhoujr\" target=\"_blank\">Source Code<\a>"
 }
