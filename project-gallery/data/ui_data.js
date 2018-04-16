6d55db36c0822db1e7f68fb61128044fe2055146
Yu Zhou <junior147147@yahoo.com>
2018-04-15 18:07:28 -0700

    figure out how to do json parsing, write a python script. done for the porfolio for now

--- project-gallery/data/ui_data.js
+++ project-gallery/data/ui_data.js
@@ -1,5 +1,5 @@
 var UI_DATA = {
-    "title": "Nekocode Open Source",
+    "title": "Yu Zhou's Portfolio",
     "github": "https://github.com/nekocode",
     "description": "I'm a full-stack engineer and specialize in android development. I like open source. Here are some projects I have contributed to the community.",
     "footer": "© Mark Yueng 2018 | <a href=\"https://github.com/nekocode/project-gallery\" target=\"_blank\">Source Code<\a>"
