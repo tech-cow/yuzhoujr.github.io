6e54bf1783aa81d71145baa769429f576723adb5
yuzhoujr <junior147147@yahoo.com>
2018-04-16 13:41:07 -0700

    time to test it out

--- temp/data/list_data.js
+++ temp/data/list_data.js
@@ -1,1 +1,120 @@
-var LIST_DATA = [{"type": "category", "name": "Python"}, {"type": "repo", "name": "LeetCode", "description": "\ud83d\udd25 Python Solutions of LeetCode Questions \ud83d\udd25", "stars": 122, "forks": 51, "lang": "Python", "url": "https://github.com/yuzhoujr/LeetCode"}, {"type": "repo", "name": "Grove", "description": "Web-app Project for CSCI 3308", "stars": 1, "forks": 0, "lang": "JavaScript", "url": "https://github.com/yuzhoujr/Grove", "types": "repo"}, {"type": "repo", "name": "wolfcoin", "description": "\ud83d\udc7b wolfcoin, a well-documented 50 liner mini-blockchain using Python \ud83d\udd17 ", "stars": 5, "forks": 0, "lang": "Python", "url": "https://github.com/yuzhoujr/wolfcoin"}, {"type": "repo", "name": "blockmaster", "description": "\u2744 a well-documented replica of Daniel van Flymen's blockchain using Python\u26a1", "stars": 5, "forks": 2, "lang": "Python", "url": "https://github.com/yuzhoujr/blockmaster"}, {"type": "repo", "name": "LovePython101", "description": "\u5341\u5c0f\u65f6\u5b66\u4f1aPython", "stars": 4, "forks": 1, "lang": "Python", "url": "https://github.com/yuzhoujr/LovePython101"}, {"type": "repo", "name": "snake", "description": "\ud83d\ude80 Refactoring Attempt of Engineerman's Snake Game \ud83d\udd30", "stars": 1, "forks": 0, "lang": "Python", "url": "https://github.com/yuzhoujr/snake"}, {"type": "repo", "name": "SimpleURL", "description": "Turn a long URL into a shorter version!", "stars": 1, "forks": 0, "lang": "Python", "url": "https://github.com/yuzhoujr/SimpleURL"}, {"type": "repo", "name": "DataMiningLoL", "description": "Final Project for CU Data Mining course", "stars": 1, "forks": 3, "lang": "Python", "url": "https://github.com/rhartsfield/DataMiningLoL"}, {"type": "category", "name": "JavaScript"}, {"types": "repo", "name": "yuzhoujr.github.io", "description": "\ud83d\uddff Progression of my ugly portfolio from its uglier past \ud83d\ude11", "stars": 4, "forks": 1, "lang": "JavaScript", "url": "https://github.com/yuzhoujr/yuzhoujr.github.io"}, {"types": "repo", "name": "mongo_bootcamp", "description": null, "stars": 1, "forks": 0, "lang": "JavaScript", "url": "https://github.com/yuzhoujr/mongo_bootcamp"}, {"type": "repo", "name": "Grove", "description": "Web-app Project for CSCI 3308", "stars": 1, "forks": 0, "lang": "JavaScript", "url": "https://github.com/yuzhoujr/Grove", "types": "repo"}]
+var LIST_DATA = [
+    {
+        "name": "Python",
+        "type": "category"
+    },
+    {
+        "description": "\ud83d\udd25 Python Solutions of LeetCode Questions \ud83d\udd25",
+        "forks": 51,
+        "lang": "Python",
+        "name": "LeetCode",
+        "stars": 122,
+        "type": "repo",
+        "url": "https://github.com/yuzhoujr/LeetCode"
+    },
+    {
+        "description": "Web-app Project for CSCI 3308",
+        "forks": 0,
+        "lang": "JavaScript",
+        "name": "Grove",
+        "stars": 1,
+        "type": "repo",
+        "types": "repo",
+        "url": "https://github.com/yuzhoujr/Grove"
+    },
+    {
+        "description": "\ud83d\udc7b wolfcoin, a well-documented 50 liner mini-blockchain using Python \ud83d\udd17 ",
+        "forks": 0,
+        "lang": "Python",
+        "name": "wolfcoin",
+        "stars": 5,
+        "type": "repo",
+        "url": "https://github.com/yuzhoujr/wolfcoin"
+    },
+    {
+        "description": "\u2744 a well-documented replica of Daniel van Flymen's blockchain using Python\u26a1",
+        "forks": 2,
+        "lang": "Python",
+        "name": "blockmaster",
+        "stars": 5,
+        "type": "repo",
+        "url": "https://github.com/yuzhoujr/blockmaster"
+    },
+    {
+        "description": "\u5341\u5c0f\u65f6\u5b66\u4f1aPython",
+        "forks": 1,
+        "lang": "Python",
+        "name": "LovePython101",
+        "stars": 4,
+        "type": "repo",
+        "url": "https://github.com/yuzhoujr/LovePython101"
+    },
+    {
+        "description": "\ud83d\ude80 Refactoring Attempt of Engineerman's Snake Game \ud83d\udd30",
+        "forks": 0,
+        "lang": "Python",
+        "name": "snake",
+        "stars": 1,
+        "type": "repo",
+        "url": "https://github.com/yuzhoujr/snake"
+    },
+    {
+        "description": "Turn a long URL into a shorter version!",
+        "forks": 0,
+        "lang": "Python",
+        "name": "SimpleURL",
+        "stars": 1,
+        "type": "repo",
+        "url": "https://github.com/yuzhoujr/SimpleURL"
+    },
+    {
+        "description": "\ud83c\udf5b A python parser using Github's REST API \ud83c\udf71  ",
+        "forks": 0,
+        "lang": "Python",
+        "name": "project-parser",
+        "stars": 1,
+        "type": "repo",
+        "url": "https://github.com/yuzhoujr/project-parser"
+    },
+    {
+        "description": "Final Project for CU Data Mining course",
+        "forks": 3,
+        "lang": "Python",
+        "name": "DataMiningLoL",
+        "stars": 1,
+        "type": "repo",
+        "url": "https://github.com/rhartsfield/DataMiningLoL"
+    },
+    {
+        "name": "JavaScript",
+        "type": "category"
+    },
+    {
+        "description": "\ud83d\uddff Progression of my ugly portfolio from its uglier past \ud83d\ude11",
+        "forks": 1,
+        "lang": "JavaScript",
+        "name": "yuzhoujr.github.io",
+        "stars": 4,
+        "types": "repo",
+        "url": "https://github.com/yuzhoujr/yuzhoujr.github.io"
+    },
+    {
+        "description": null,
+        "forks": 0,
+        "lang": "JavaScript",
+        "name": "mongo_bootcamp",
+        "stars": 1,
+        "types": "repo",
+        "url": "https://github.com/yuzhoujr/mongo_bootcamp"
+    },
+    {
+        "description": "Web-app Project for CSCI 3308",
+        "forks": 0,
+        "lang": "JavaScript",
+        "name": "Grove",
+        "stars": 1,
+        "type": "repo",
+        "types": "repo",
+        "url": "https://github.com/yuzhoujr/Grove"
+    }
+]
