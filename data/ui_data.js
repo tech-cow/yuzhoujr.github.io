e6b0d110741dac5784c93febde394586b0f93602
yuzhoujr <junior147147@yahoo.com>
2018-04-16 17:27:32 -0700

    totally bias

--- data/ui_data.js
+++ data/ui_data.js
@@ -1,6 +1,6 @@
 var UI_DATA = {
     "title": "",
     "github": "https://github.com/yuzhoujr",
-    "description": "self.name = Yu_Zhou. a Software Engineer @ Synopsys. I love open source and python. Here are some side-projects I have working on for fun.",
+    "description": "self.name = Yu_Zhou. a Software Engineer @ Synopsys. Life is short, so please use python and js a lot. I am totally not bias.",
     "footer": "© Yu Zhou 2018 | <a href=\"https://github.com/yuzhoujr\" target=\"_blank\">Source Code<\a>"
 }
