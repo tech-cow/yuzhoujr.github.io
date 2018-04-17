433476b597850b798bd6d5cb3d1afcab575579fa
yuzhoujr <junior147147@yahoo.com>
2018-04-16 17:16:32 -0700

    yo

--- data/ui_data.js
+++ data/ui_data.js
@@ -1,6 +1,6 @@
 var UI_DATA = {
     "title": "",
     "github": "https://github.com/yuzhoujr",
-    "description": "I'm a Software Engineer @ Synopsys. I love open source and python. Here are some side-projects I have working on for fun.",
+    "description": "self.name = Yu_Zhou. a Software Engineer @ Synopsys. I love open source and python. Here are some side-projects I have working on for fun.",
     "footer": "© Yu Zhou 2018 | <a href=\"https://github.com/yuzhoujr\" target=\"_blank\">Source Code<\a>"
 }
