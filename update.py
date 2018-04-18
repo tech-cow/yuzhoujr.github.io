2bb481b52af5a7248bd5555687d0a9ca3dfd1b01
Yu Zhou <junior147147@yahoo.com>
2018-04-17 21:21:41 -0700

    auto push at 2018-04-17 21:21:41.350430

--- update.py
+++ update.py
@@ -6,5 +6,5 @@ import datetime
 
 subprocess.call(["python3", "data/parser.py"])
 subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())]) # 加上当前系统的时间
+subprocess.call(["git", "commit", "-m", "auto push at " + str(datetime.datetime.now())])
 subprocess.call(["git", "push"])
