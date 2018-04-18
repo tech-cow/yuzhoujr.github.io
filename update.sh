50d8eda2bad1afa8247fb37eb85b1cd141110f86
yuzhoujr <you@example.com>
2018-04-17 21:37:00 -0700

    2018-04-17 21:37:00

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+COMMIT ="auto push at "
+TIME=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Executing GitHub API parser to feed JSON data to the frontend
+python data/parser.py
+
+# Automate Git
+git remote -v
+git add .
+git commit -m "$COMMIT$TIME"
+git push origin master
+
+# Crontab Command
+# */1 * * * * /Users/yui/Dropbox/github/portfolio/update.sh > /var/log/myjob.log 2>&1
