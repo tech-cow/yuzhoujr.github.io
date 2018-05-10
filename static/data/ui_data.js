e9df98138f3e38fa32e057fd7f41dc1fe8f15b54
yuzhoujr <junior147147@yahoo.com>
2018-05-10 08:41:18 -0700

    finally, my site can count visitor's status

--- static/data/ui_data.js
+++ static/data/ui_data.js
@@ -1,6 +1,6 @@
 var UI_DATA = {
-    "title": "",
-    "github": "https://github.com/yuzhoujr",
-    "description": "self.name = Yu_Zhou. a Software Engineer @ Synopsys. Life is short, so please use python and js a lot. I am totally not bias.",
-    "footer": "© Yu Zhou 2018 | <a href=\"https://github.com/yuzhoujr\" target=\"_blank\">Source Code<\a>"
+  "title": "",
+  "github": "https://github.com/yuzhoujr",
+  "description": "self.name = Yu_Zhou. a Software Engineer @ Synopsys. Life is short, so please use python and js a lot. I am totally not bias.",
+  "footer": "© Yu Zhou 2018 | <a href=\"https://github.com/yuzhoujr\" target=\"_blank\">Source Code<\a>"
 }
