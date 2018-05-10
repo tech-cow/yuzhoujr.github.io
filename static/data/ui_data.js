d32dd93c16d7945e567801a374d5ec230b93d7ca
yuzhoujr <junior147147@yahoo.com>
2018-05-10 08:57:55 -0700

    love

--- static/data/ui_data.js
+++ static/data/ui_data.js
@@ -2,5 +2,5 @@ var UI_DATA = {
   "title": "",
   "github": "https://github.com/yuzhoujr",
   "description": "self.name = Yu_Zhou. a Software Engineer @ Synopsys. Life is short, so please use python and js a lot. I am totally not bias.",
-  "footer": "© Yu Zhou 2018 | <a href=\"https://github.com/yuzhoujr\" target=\"_blank\">Source Code<\a>"
+  "footer": '© 2015 - 2018 <span class="with-love" id="animate"><i class="fa fa-heart"></i></span>  <a href=\"https://github.com/yuzhoujr\" target=\"_blank\">Yu Zhou<\a>'
 }
