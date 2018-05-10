c713ecd0f4cf0be5349c621c207fffcb6403420e
yuzhoujr <junior147147@yahoo.com>
2018-05-10 09:04:33 -0700

    very greedy way to spacing out... more like lazy as hell

--- static/data/ui_data.js
+++ static/data/ui_data.js
@@ -2,5 +2,5 @@ var UI_DATA = {
   "title": "",
   "github": "https://github.com/yuzhoujr",
   "description": "self.name = Yu_Zhou. a Software Engineer @ Synopsys. Life is short, so please use python and js a lot. I am totally not bias.",
-  "footer": '© 2015 - 2018 <span class="with-love" id="animate"><i class="fa fa-heart"></i></span>  <a href=\"https://github.com/yuzhoujr\" target=\"_blank\">Yu Zhou<\a>'
+  "footer": '© 2015 - 2018 &nbsp<span class="with-love" id="animate"><i class="fa fa-heart"></i></span> <a href=\"https://github.com/yuzhoujr\" target=\"_blank\">&nbspYu Zhou<\a>'
 }
